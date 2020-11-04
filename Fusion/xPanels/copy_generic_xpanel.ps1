5458a3fac9cbb789cb897d315b96ee2bf5cb3c3c
Amy Batzel <tbatzel@uw.edu>
2020-11-04 13:04:32 -0800

    Update copy_generic_xpanel.ps1
    Now a script that can be called with parameters

--- Fusion/xPanels/copy_generic_xpanel.ps1
+++ Fusion/xPanels/copy_generic_xpanel.ps1
@@ -1,51 +1,71 @@
-#Config
-$targetBuildingCode = "UW"
-$targetRoomNumber = "2020"
-$targetHost = "172.25.0.1"
-$targetIPID = "15"
+param (
+    [Parameter(Mandatory)][ValidatePattern("^[0-9A-Z]{2,8}$")]$bld, 
+    [Parameter(Mandatory)][ValidatePattern("^[0-9A-Z]{2,8}$")]$rm, 
+    [Parameter(Mandatory)][ValidatePattern("\b((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)(\.|$)){4}\b")]$ip, 
+    [Parameter(Mandatory)][ValidatePattern("^[0-9A-F]{2}$")]$ipid,
+    [ValidateSet('DMPS3-300-C', 'fake')]$type="DMPS3-300-C",
+    [switch]$force = $false
+)
 
-#Defaults
-$pathRoot = "C:\xPanels\"
-$zipFileSource = "zzzGenericDMPS.c3p"
-$fileToEdit = "swf/Environment.xml"
-$zipFile = $targetBuildingCode + "_" + $targetRoomNumber + ".c3p"
+
+
+#defaults / setup
+$path = "c:\xPanels"
+$genFolder = "\generic"
+$fileExt = "c3p"
+$fileConfig = "swf/Environment.xml"
+Add-Type -Assembly 'System.IO.Compression.FileSystem'
+$compressionLevel = [System.IO.Compression.CompressionLevel]::Fastest
+$fileConfigTemp = $path + "\" + [guid]::NewGuid().ToString() + ".xml"                # make this a tmp file and check
+
+
+
+#Source xPanel
+$zipSrc = $path + $genFolder + "\" + $type + "." + $fileExt
+if (-not [System.IO.File]::Exists($zipSrc))
+{
+    write-host "Error: Source File Not Found: $zipSrc"
+    exit
+}
+
+#Destination xPanel
+$zipDst = $path +"\" + $bld + "_" + $rm + "." + $fileExt
+if (    ([System.IO.File]::Exists($zipDst)) -and (-not $force) )
+{
+    write-host "Error: Destination File Exists: $zipDst, use -force"
+    exit
+}
 
 
 
 #Copy Generic Panel File
-Copy-Item "$pathRoot$zipFileSource" -Destination "$pathRoot$zipFile"  -Force
+Copy-Item $zipSrc -Destination $zipDst  -Force
 
-#Define Compression Options
-Add-Type -Assembly 'System.IO.Compression.FileSystem'
-$compressionLevel = [System.IO.Compression.CompressionLevel]::Fastest
-$zipFilePath = $pathRoot + $zipFile
 
-#Extract Environment.xml
-$zip = [System.IO.Compression.ZipFile]::Open($zipFilePath, 'update')
-$entry = $zip.Entries | Where-Object { $_.FullName -eq $fileToEdit }
-$fileName = $entry.Name
-[System.IO.Compression.ZipFileExtensions]::ExtractToFile($entry, "$pathRoot$fileName", $true)
+#Extract Config File
+$zip = [System.IO.Compression.ZipFile]::Open($zipDst, 'update')
+$entry = $zip.Entries | Where-Object { $_.FullName -eq $fileConfig }
+[System.IO.Compression.ZipFileExtensions]::ExtractToFile($entry, $fileConfigTemp, $true)
 
-#Update Environment.xml
-[xml]$xmlDoc = Get-Content "$pathRoot$fileName"
-$xmlDoc.Crestron.Properties.CNXConnection.Host.'#cdata-section' = $targetHost
-$xmlDoc.Crestron.Properties.CNXConnection.IPID.'#cdata-section' = $targetIPID
-$xmlDoc.Save("$pathRoot$fileName")
+#Update Config File
+[xml]$xmlDoc = Get-Content $fileConfigTemp
+$xmlDoc.Crestron.Properties.CNXConnection.Host.'#cdata-section' = $ip
+$xmlDoc.Crestron.Properties.CNXConnection.IPID.'#cdata-section' = $ipid
+$xmlDoc.Save($fileConfigTemp)
 
 #Delete existing entry from ZIP
 $entry.Delete()
 
 #Add new file to ZIP
-[System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, "$pathRoot\$fileName", $fileToEdit)
+$clean = [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($zip, $fileConfigTemp, $fileConfig)
 
 #Cleanup
-Remove-Item -Path "$pathRoot\$fileName"
+Remove-Item -Path $fileConfigTemp
 $zip.Dispose()
 
 
+
+
+
 #Add To RemoteApp Collection
-$appDisplayName = $targetBuildingCode + " " + $targetRoomNumber
-New-RDRemoteApp -CollectionName "Admin Tools" -DisplayName $appDisplayName -FilePath "C:\Program Files (x86)\Crestron\XPanel\CrestronXPanel\CrestronXPanel.exe" -FolderName  "xPanel" -CommandLineSetting Require -RequiredCommandLine $zipFilePath
-
-
-
+$clean = New-RDRemoteApp -CollectionName "Admin Tools" -DisplayName "$bld $rm" -FilePath "C:\Program Files (x86)\Crestron\XPanel\CrestronXPanel\CrestronXPanel.exe" -FolderName  "xPanel" -CommandLineSetting Require -RequiredCommandLine $zipDst
